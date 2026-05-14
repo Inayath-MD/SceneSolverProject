a5696010895ac79c5abfbbf61ee77cb9c0ef1f33
MohammedInayath5196 <mdinayath.2005@gmail.com>
2026-05-14 12:10:17 +0530

    fixed unused imports

--- Frontendnew/src/components/SignUpPage.js
+++ Frontendnew/src/components/SignUpPage.js
@@ -4,7 +4,8 @@ import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import styles from './Auth.module.css';
 import background from '../assets/login.jpg';
-import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa';
+// import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa';
+// import { FaUser } from "react-icons/fa";
 
 const SignUpPage = () => {
     // --- NEW: Add state for name ---
